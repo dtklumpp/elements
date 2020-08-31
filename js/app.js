96c7140200871d927b2edb50332e63b197371626
dtklumpp <dtklumpp@gmail.com>
2020-08-31 16:10:27 -0400

    added board and made some squares

--- js/app.js
+++ js/app.js
@@ -4,3 +4,9 @@ const fxn1 = function() {
     console.log('clicked button 1');
 }
 $('#button1').on('click', fxn1);
+
+const board1 = $('#board');
+for(i=1; i<=3; i++){
+    const newSquare = $('<div/>').addClass('square');
+    board1.append(newSquare);
+}
