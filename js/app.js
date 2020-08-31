f23d47a37d0ae4d12cc3d8f961a09e3672400e50
dtklumpp <dtklumpp@gmail.com>
2020-08-31 16:16:57 -0400

    made square board

--- js/app.js
+++ js/app.js
@@ -6,7 +6,11 @@ const fxn1 = function() {
 $('#button1').on('click', fxn1);
 
 const board1 = $('#board');
-for(i=1; i<=3; i++){
-    const newSquare = $('<div/>').addClass('square');
-    board1.append(newSquare);
+for(i=1; i<=5; i++){
+    for(j=1; j<=5; j++){
+        const newSquare = $('<div/>').addClass('square');
+        newSquare.attr('x-coord', j);
+        newSquare.attr('y-coord', i);
+        board1.append(newSquare);
+    }
 }
