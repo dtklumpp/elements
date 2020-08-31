926e9894328395b15004e89aed454fefff345974
dtklumpp <dtklumpp@gmail.com>
2020-08-31 17:59:25 -0400

    added one random element stone

--- js/app.js
+++ js/app.js
@@ -23,13 +23,29 @@ const clickSquare = function(event){
 
 $('.square').on('click', clickSquare);
 
+//replace this with a class factory
 const piece1 = $('<div/>').addClass('piece');
 piece1.attr('piecetype', 'sage');
 const sage1 = {
     icon: piece1
 };
+piece1.attr('base-color', 'black');
+
+const piece2 = $('<div/>').addClass('piece');
+piece2.attr('piecetype', 'sage');
+const sage2 = {
+    icon: piece2
+};
+piece2.attr('base-color', 'white');
+piece2.css('top', '5px');
+piece2.css('left', '10px');
+piece2.css('background-color', 'white');
+
+
+
 
 $('#play-area').append(sage1.icon);
+$('#play-area').append(sage2.icon);
 
 let activePiece;
 
@@ -42,24 +58,49 @@ const startAction = function(event){
     activePiece.css('background-color', '#a10000')
 }
 
-piece1.on('click', startAction);
 
 const endAction = function(event){
     console.log('end action begin');
     if(activePiece){
-        let targetVar = $(event.target);
-        if(targetVar.attr('class') === 'square'){
-            x1 = targetVar.attr('x-coord');
-            y1 = targetVar.attr('y-coord');
-            base1 = activePiece.baseColor;
-            activePiece.css('left', (5+(x1*81))+'px');
-            activePiece.css('top', (10+(y1*79))+'px');
-            //activePiece.css('background-color', base1);
-            activePiece.css('background-color', 'black');
-            console.log(activePiece);
-            activePiece = null;
-        }
+        //if(activePiece.attr('piecetype') === 'sage'){
+            let targetVar = $(event.target);
+            if(targetVar.attr('class') === 'square'){
+                x1 = targetVar.attr('x-coord');
+                y1 = targetVar.attr('y-coord');
+                base1 = activePiece.attr('base-color');
+                activePiece.css('left', (5+(x1*81))+'px');
+                activePiece.css('top', (10+(y1*79))+'px');
+                activePiece.css('background-color', base1);
+                //activePiece.css('background-color', 'black');
+                console.log(activePiece);
+                activePiece = null;
+            }
+        //}
     }
 }
 
 $('.square').on('click', endAction);
+
+const bag1 = [];
+const colors1 = ['blue', 'grey', 'brown', 'orange']
+for(eachColor of colors1){
+    for(i=1; i<=15; i++){
+        const element1 = $('<div/>').addClass('piece stone');
+        element1.css('background-color', eachColor);
+        element1.addClass(eachColor);
+        element1.attr('base-color', eachColor);
+        bag1.push(element1);
+    }
+}
+indexRand = Math.floor(Math.random()*bag1.length)
+const randDraw = bag1[indexRand];
+console.log(randDraw);
+randDraw.css('top', '5px');
+randDraw.css('left', '335px');
+$('#play-area').append(randDraw);
+
+
+$('.piece').on('click', startAction);
+
+//const piece1 = $('<div/>').addClass('piece');
+//piece1.attr('piecetype', 'sage');
