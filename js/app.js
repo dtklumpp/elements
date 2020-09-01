9cb66f92f9f99c033e2380a5a363a43d9a53f871
dtklumpp <dtklumpp@gmail.com>
2020-08-31 22:13:32 -0400

    prevented sages from moving more than one space.  also refactored some stuff.  let's see.  made functions getCoords, getDistance, resetActivePiece.  probably added drawStones function and put them in the panel, unless that was a previous commit

--- js/app.js
+++ js/app.js
@@ -71,6 +71,46 @@ const startAction = function(event){
 }
 
 
+/* 
+const movePiece = function(startPiece, endSquare){
+    x1 = endSquare.attr('x-coord');
+    y1 = endSquare.attr('y-coord');
+    endSquare.append(activePiece);
+} */
+
+const getCoords = function(jqEl){
+    if(jqEl.attr('class') = 'square'){
+        x1 = jqEl.attr('x-coord');
+        y1 = jqEl.attr('y-coord');
+    }
+    else{
+        x1 = jqEl.parent().attr('x-coord');
+        y1 = jqEl.parent().attr('y-coord');
+    }
+    return {
+        x1: x1,
+        y1: y1
+    };
+}
+
+const getDistance = function(piece1, square1){
+    x1 = piece1.parent().attr('x-coord');
+    y1 = piece1.parent().attr('y-coord');
+    x2 = square1.attr('x-coord');
+    y2 = square1.attr('y-coord');
+    console.log(x1, x2, y1, y2);
+    return (Math.sqrt((x1-x2)**2 + (y1-y2)**2));
+}
+
+const resetActivePiece = function(){
+    base1 = activePiece.attr('base-color');
+    activePiece.css('background-color', base1);
+    //activePiece.css('background-color', 'black');
+    //console.log(activePiece);
+    activePiece = null;
+}
+
+
 const endAction = function(event){
     console.log('end action begin');
     let targetVar = $(event.target);
@@ -78,42 +118,72 @@ const endAction = function(event){
     if(activePiece
         && targetVar.attr('class') === 'square'
         && targetVar.children().length === 0){
+
+            if(activePiece.attr('piecetype') === 'sage'){
+                //console.log(getCoords(targetVar).x1);
+                //console.log(getCoords(targetVar).y1);
+                const dist1 = getDistance(activePiece,targetVar);
+                console.log(dist1);
+                if(dist1 < 1.5){
+                    targetVar.append(activePiece);
+                    resetActivePiece();
+                }
+            }
+            else {
+                    targetVar.append(activePiece);
+                    resetActivePiece();
+            }
+
+
+            //movePiece(activePiece, targetVar);
             //if(activePiece.attr('piecetype') === 'sage'){
-            x1 = targetVar.attr('x-coord');
-            y1 = targetVar.attr('y-coord');
-            targetVar.append(activePiece);
-            activePiece.css('left', '5px');
-            activePiece.css('top', '5px');
+           ////// x1 = targetVar.attr('x-coord');
+           ////// y1 = targetVar.attr('y-coord');
+           ///////////targetVar.append(activePiece);
+            //activePiece.css('left', '5px');
+            //activePiece.css('top', '5px');
             //move to position of clicked square
             //activePiece.css('left', (5+(x1*81))+'px');
             //activePiece.css('top', (10+(y1*79))+'px');
-            base1 = activePiece.attr('base-color');
-            activePiece.css('background-color', base1);
+        //base1 = activePiece.attr('base-color');
+        //activePiece.css('background-color', base1);
             //activePiece.css('background-color', 'black');
             //console.log(activePiece);
-            activePiece = null;
+        //activePiece = null;
             
         //}
     }
 }
-
-
-
 $('.square').on('click', endAction);
 
+
+
+
+
+
+
+
+
+
 const bag1 = [];
-const colors1 = ['blue', 'grey', 'brown', 'orange']
-for(eachColor of colors1){
+//const colors1 = ['blue', 'grey', 'brown', 'orange']
+const elements1 = [
+    {name: 'water', color: 'blue'},
+    {name: 'air', color: 'grey'},
+    {name: 'earth', color: 'brown'},
+    {name: 'fire', color: 'orange'}
+]
+for(eachType of elements1){
     for(i=1; i<=15; i++){
-        const element1 = $('<div/>').addClass('piece stone');
-        element1.css('background-color', eachColor);
-        element1.addClass(eachColor);
-        element1.attr('base-color', eachColor);
-        bag1.push(element1);
+        const stone1 = $('<div/>').addClass('piece stone');
+        stone1.css('background-color', eachType.color);
+        stone1.attr('base-color', eachType.color);
+        stone1.addClass(eachType.name);
+        bag1.push(stone1);
     }
 }
 indexRand = Math.floor(Math.random()*bag1.length)
-const randDraw = bag1[indexRand];
+const randDraw = bag1.splice(indexRand, 1)[0];
 //console.log(randDraw);
 //randDraw.css('top', '5px');
 //randDraw.css('left', '335px');
@@ -122,8 +192,32 @@ $('#4').append(randDraw);
 //$('#play-area').append(randDraw);
 
 
-$('.piece').on('click', startAction);
 
+
+
+//make fxn of n?
+const drawStones = function(){
+    console.log('clicked button 2');
+    for(i = 0; i < 5; i++){
+        index1 = Math.floor(Math.random()*bag1.length);
+        newStone = bag1.splice(index1,1)[0];
+        newStone.css('position', 'relative');
+        $('#panel').append(newStone);
+
+    }
+    //omg this is so WET i just copy-pasted
+    $('.piece').on('click', startAction);
+}
+
+$('#button2').on('click', drawStones);
+
+
+
+
+
+
+
+$('.piece').on('click', startAction);
 //const piece1 = $('<div/>').addClass('piece');
 //piece1.attr('piecetype', 'sage');
 
